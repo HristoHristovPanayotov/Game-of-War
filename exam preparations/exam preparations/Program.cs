99844aee2a222a359617437055a6f56cf30203a1
HristoHristovPanayotov <emailformyself123123@gmail.com>
2023-12-22 23:47:23 +0200

    Update Program.cs

--- exam preparations/exam preparations/Program.cs
+++ exam preparations/exam preparations/Program.cs
@@ -1,10 +1,47 @@
-﻿namespace exam_preparations
+﻿using System;
+
+namespace Exam_Preparatiions
+
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double energy = double.Parse(Console.ReadLine());
+            int wins = 0;
+
+            string command = Console.ReadLine();
+
+            while (command != "End of battle")
+            {
+                double distance = double.Parse(command);
+
+                if (energy < distance)
+                {
+                    Console.WriteLine($"Not enough energy! Game ends with {wins} won battles and {energy} energy");
+                    energy -= distance; // Subtract energy before breaking the loop
+                    break;
+                }
+
+                energy -= distance;
+                wins++;
+
+                if (wins % 3 == 0)
+                {
+                    energy += wins;
+                }
+
+                command = Console.ReadLine();
+            }
+
+            if (energy >= 0)
+            {
+                Console.WriteLine($"Won battles: {wins}. Energy left: {energy}");
+            }
+            else
+            {
+                Console.WriteLine($"Aleksandar, please be careful");
+            }
         }
     }
 }
